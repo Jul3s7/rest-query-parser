20471925726404b17941d72d8c5925031a916b7c
Julia Langhammer <julia.langhammer7@gmail.com>
2025-08-05 17:48:15 +0200

    add tests for NULL and empty string handling

--- filter_test.go
+++ filter_test.go
@@ -48,6 +48,166 @@ func Test_Args(t *testing.T) {
 	})
 }
 
+func Test_NullIntegerHandling(t *testing.T) {
+	t.Run("Integer with IS NULL method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "id[is]",
+			Name:   "id",
+			Method: IS,
+			Value:  NULL,
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "(id IS NULL OR id IS '')", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{NULL}, args)
+	})
+
+	t.Run("Integer with NOT NULL method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "id[not]",
+			Name:   "id",
+			Method: NOT,
+			Value:  NULL,
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "(id IS NOT NULL OR id IS NOT '')", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{NULL}, args)
+	})
+
+	t.Run("Integer with IS method but non-NULL value should fail", func(t *testing.T) {
+		filter := Filter{
+			Key:    "id[is]",
+			Name:   "id",
+			Method: IS,
+			Value:  123,
+		}
+		_, err := filter.Where()
+		assert.Equal(t, err, ErrUnknownMethod)
+
+		_, err = filter.Args()
+		assert.Equal(t, err, ErrUnknownMethod)
+	})
+
+	t.Run("Integer with NOT method but non-NULL value should fail", func(t *testing.T) {
+		filter := Filter{
+			Key:    "id[not]",
+			Name:   "id",
+			Method: NOT,
+			Value:  123,
+		}
+		_, err := filter.Where()
+		assert.Equal(t, err, ErrUnknownMethod)
+
+		_, err = filter.Args()
+		assert.Equal(t, err, ErrUnknownMethod)
+	})
+}
+
+func Test_EmptyStringHandling(t *testing.T) {
+	t.Run("Empty string with EQ method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[eq]",
+			Name:   "name",
+			Method: EQ,
+			Value:  "",
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name = ?", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{""}, args)
+	})
+
+	t.Run("Empty string with NE method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[ne]",
+			Name:   "name",
+			Method: NE,
+			Value:  "",
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name != ?", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{""}, args)
+	})
+
+	t.Run("Empty string with LIKE method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[like]",
+			Name:   "name",
+			Method: LIKE,
+			Value:  "",
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name LIKE ?", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{""}, args)
+	})
+
+	t.Run("Empty string with ILIKE method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[ilike]",
+			Name:   "name",
+			Method: ILIKE,
+			Value:  "",
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name ILIKE ?", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{""}, args)
+	})
+
+	t.Run("Empty string with IN method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[in]",
+			Name:   "name",
+			Method: IN,
+			Value:  []string{"", "test", ""},
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name IN (?, ?, ?)", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{"", "test", ""}, args)
+	})
+
+	t.Run("Empty string with NIN method", func(t *testing.T) {
+		filter := Filter{
+			Key:    "name[nin]",
+			Name:   "name",
+			Method: NIN,
+			Value:  []string{"", "excluded"},
+		}
+		where, err := filter.Where()
+		assert.NoError(t, err)
+		assert.Equal(t, "name NOT IN (?, ?)", where)
+
+		args, err := filter.Args()
+		assert.NoError(t, err)
+		assert.Equal(t, []interface{}{"", "excluded"}, args)
+	})
+}
+
 func Test_RemoveOrEntries(t *testing.T) {
 	type testCase struct {
 		name           string
